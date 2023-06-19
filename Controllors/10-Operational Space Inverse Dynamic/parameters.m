784a840f4e701002401be8a836957c1c682e9006
Monruethai Sueksakan <86582058+Mimigatto@users.noreply.github.com>
2023-06-19 12:25:17 +0200

    Add files via upload

--- Controllors/10-Operational Space Inverse Dynamic/parameters.m
+++ Controllors/10-Operational Space Inverse Dynamic/parameters.m
@@ -1,1 +1,25 @@
+%sine parameters
+A = 1;
+Fc = 1;
 
+%lengths
+lenghts_values = [[0.02, 0.1256, 0.4]; 
+                  [0.03, 0.03, 0.3]; 
+                  [0.02, 0.1256, 0.16]];
+density = 2700; %Almunium density
+mass_values = [pi * lenghts_values(1,1)^2*lenghts_values(1,3)*density, lenghts_values(2,1)*lenghts_values(2,2)*lenghts_values(3,3)*density, lenghts_values(3,1)^2*lenghts_values(3,3)*density];
+           
+
+%masses
+params.m = mass_values;
+
+%gravity
+params.g = 9.806;
+
+%Friction
+params.Fv = [0,0,0];
+
+%PD control values
+Kp = diag([280,280,70,70,70,70]);
+
+Kd = diag([30,30,20,20,20,20]);
